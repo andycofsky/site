9f58bca8a4de547976a8df7b2fbe9a117fa0311a
ajl1152 <andrew.litcofsky@unh.edu>
2025-06-14 19:28:52 -0400

    Created basic Vite site with React and with TailwindCSS installed. Flushed out starter code.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import loadingIcon from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-screen h-screen flex justify-center items-center bg-conic-180 from-cyan-200 via-indigo-50 to-cyan-200'>
+        <div className='mt-4 justify-items-center'>
+          <img src={loadingIcon} className='size-32 animate-spin' />
+          <p className='mt-4 font-semibold text-cyan-950'>⚠️ SITE BUILDING IN PROGRESS ⚠️</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
